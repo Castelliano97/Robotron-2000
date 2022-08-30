9da56b3986be0e325c809810ee7e93b0109e1a72
Castelliano97 <matheus_castelliano@outlook.com>
2022-08-30 18:34:09 -0300

    finalizei o projeto

--- main.js
+++ main.js
@@ -2,23 +2,65 @@
 // const subtrair = document.querySelectorAll("#subtrair");
 // const somar = document.querySelectorAll("#somar");
 
-const controle = document.querySelectorAll(".controle-ajuste")
+const controle = document.querySelectorAll("[data-controle]")
+const estatistica = document.querySelectorAll("[data-estatistica]")
+
+const pecas = {
+    "bracos": {
+        "forca": 29,
+        "poder": 35,
+        "energia": -21,
+        "velocidade": -5
+    },
+
+    "blindagem": {
+        "forca": 41,
+        "poder": 20,
+        "energia": 0,
+        "velocidade": -20
+    },
+    "nucleos":{
+        "forca": 0,
+        "poder": 7,
+        "energia": 48,
+        "velocidade": -24
+    },
+    "pernas":{
+        "forca": 27,
+        "poder": 21,
+        "energia": -32,
+        "velocidade": 42
+    },
+    "foguetes":{
+        "forca": 0,
+        "poder": 28,
+        "energia": 0,
+        "velocidade": -2
+    }
+}
 
 controle.forEach( (elemento)=>{
     elemento.addEventListener("click", (evento)=>{
-        manipulaDados(evento.target.textContent)})
+        manipulaDados(evento.target.dataset.controle, evento.target.parentNode)
+        atualizaEstatistica(evento.target.dataset.peca)
+    })
+
 })
 
-// Partes
-const bracos = document.querySelector("#bracos");
-
-function manipulaDados (operacao){
+function manipulaDados (operacao, valor){
+    const peca = valor.querySelector("[data-contador]");
     if (operacao === "-"){
-        bracos.value = parseInt(bracos.value) - 1;
+        peca.value = parseInt(peca.value) - 1;
     }
     else{
-        bracos.value = parseInt(bracos.value) + 1;
+        peca.value = parseInt(peca.value) + 1;
     }
+}
+
+function atualizaEstatistica (peca){
+    estatistica.forEach((elemento)=>{
+        elemento.textContent = parseInt(elemento.textContent) + pecas[peca][elemento.dataset.estatistica]
+    })
 }
 
 
