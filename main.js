f7aa850291612d53a25e250d7eb127e89cf3eade
Castelliano97 <matheus_castelliano@outlook.com>
2022-08-30 17:05:28 -0300

    ja consigo somar e subtrair usando o array dos controles

--- main.js
+++ main.js
@@ -1,7 +1,28 @@
-const robotron = document.querySelector('#robotron');
+// Variaveis de botoes
+// const subtrair = document.querySelectorAll("#subtrair");
+// const somar = document.querySelectorAll("#somar");
 
-robotron.addEventListener("click", dizOi);
+const controle = document.querySelectorAll(".controle-ajuste")
 
-function dizOi () {
-    console.log('po');
+controle.forEach( (elemento)=>{
+    elemento.addEventListener("click", (evento)=>{
+        manipulaDados(evento.target.textContent)})
+})
+
+// Partes
+const bracos = document.querySelector("#bracos");
+
+function manipulaDados (operacao){
+    if (operacao === "-"){
+        bracos.value = parseInt(bracos.value) - 1;
+    }
+    else{
+        bracos.value = parseInt(bracos.value) + 1;
+    }
 }
+
+
+// Criei constantes subtrair e somar para selecionar os botões mas acabei não precisando dela
+// a constante controle serve para listar todas as vezes que os botoes de controle aparecem
+// depois usei a forEach pra "toda vez que eu" clicar num dos elementos do array controle eu chamar a funcao manipulaDados
+// evento.target.textContent serve pra mostrar o texto que tem dentro daquela tag daquele item do array
